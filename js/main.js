ca42f8f75f676ba798486015fbc7f5a0453cbe2a
Kathy Sun <kathy.shirong@gmail.com>
2015-01-31 02:01:47 -0500

    recipe cards + member photos

--- js/main.js
+++ js/main.js
@@ -22,3 +22,20 @@ $('.recipe')
     $(this).find('p').css("color", "#000");
     $(this).css("background-color", "#fff");
 });
+
+// smooth scrolling
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 3
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
